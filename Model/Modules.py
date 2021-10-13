c29e81167d23266e0c12e06afbd8f1a9197165b8
xlbryantx <xlbryant@stu.xjtu.edu.cn>
2021-10-13 11:53:08 +0800

    update

--- Model/Modules.py
+++ Model/Modules.py
@@ -20,23 +20,3 @@ class ScaledDotProductAttention(nn.Module):
         output = torch.matmul(attn, v)
 
         return output, attn
-
-
-# class ScaledDotProductTimeAttention(nn.Module):
-#     ''' Scaled Dot-Product Attention '''
-#
-#     def __init__(self, temperature, d_k, attn_dropout=0.1):
-#         super().__init__()
-#         self.temperature = temperature
-#         self.dropout = nn.Dropout(attn_dropout)
-#
-#     def forward(self, q, k, v, mask=None):
-#         attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
-#
-#         if mask is not None:
-#             attn = attn.masked_fill(mask == 0, -1e9)
-#
-#         attn = self.dropout(F.softmax(attn, dim=-1))
-#         output = torch.matmul(attn, v)
-#
-#         return output, attn
